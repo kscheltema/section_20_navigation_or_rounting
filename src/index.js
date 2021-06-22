1d0b4f9376b7d8468afab920dd23d418d31c7ef2
kscheltema <k87scheltema@outlook.com>
2021-06-22 19:34:05 +0200

    css imports

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
+import './index.module.css';
 import App from './App';
 
 ReactDOM.render(
