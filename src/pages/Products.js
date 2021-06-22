d1b2e8cab7ff88aed59939d6fbff52cbda391b34
kscheltema <k87scheltema@outlook.com>
2021-06-22 21:38:31 +0200

    dynamic routes for Route

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,5 +1,11 @@
 const Products = () => {
-return <h1>Products Page!</h1>
+return <section><h1>Products Page!</h1>
+<ul>
+    <li>Product 1</li>
+    <li>Product 2</li>
+    <li>Product 3</li>
+</ul>
+</section>
 };
 
 export default Products;
