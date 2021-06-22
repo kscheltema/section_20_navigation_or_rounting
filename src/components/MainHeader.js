bac77ae57af9abc399b429765c347d2c47618e8e
kscheltema <k87scheltema@outlook.com>
2021-06-22 19:18:04 +0200

    Link prevents a rerender unlike a tag

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -1,12 +1,14 @@
+import {Link} from 'react-router-dom'; 
+
 const MainHeader = () => {
     return <header>
         <nav>
             <ul>
                 <li>
-<a href='/welcome'>Welcome</a>
+<Link to='/welcome'>Welcome</Link>
                 </li>
                 <li>
-                    <a href='/products'>Products</a>
+                    <Link to='/products'>Products</Link>
                 </li>
             </ul>
         </nav>
