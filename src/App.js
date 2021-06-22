ee251db0d8714dcb39fee9950419456e2c7eaec9
kscheltema <k87scheltema@outlook.com>
2021-06-22 19:09:10 +0200

    routes with link navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import {Route} from 'react-router-dom';
 import Welcome from './pages/Welcome';
 import Products from './pages/Products';
+import MainHeader from './components/MainHeader';
 
 function App() {
   return (
     <div>
-      <header>    place to store the navigation         </header>
+<MainHeader />
       <main>          place to store the routes         </main>
 <Route path='/welcome'>
 <Welcome />  
