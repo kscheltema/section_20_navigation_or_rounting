b091b8aaafe8da2b4fda26df000a60236bddf5d3
kscheltema <k87scheltema@outlook.com>
2021-06-22 18:01:28 +0200

    React Routing Basics

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import {Route} from 'react-router-dom';
+import Welcome from './Components/Welcome';
+import Products from './Components/Products';
+
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+<Route path='/welcome'>
+<Welcome />  
+   </Route>
+   <Route path='/products'>
+<Products />
+   </Route>
     </div>
   );
-  
 }
 
 export default App;
