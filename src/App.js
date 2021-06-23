89e8fcf0c6535c7dc5c229482fd4dc0ccb3b2218
kscheltema <k87scheltema@outlook.com>
2021-06-23 13:58:22 +0200

    the alternative Redirect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Switch } from "react-router-dom";
+import { Redirect, Route, Switch } from "react-router-dom";
 import Welcome from "./pages/Welcome";
 import Products from "./pages/Products";
 import MainHeader from "./components/MainHeader";
@@ -11,6 +11,9 @@ function App() {
       <main>
         <Switch>
           <Route path="/" exact>
+            <Redirect to="/welcome" />
+          </Route>
+          <Route path="/welcome">
             <Welcome />
           </Route>
           <Route path="/products" exact>
